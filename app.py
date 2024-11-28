dd4742b8232a78e619d337cb128d72d744fcc24f
rahul884061 <146861062+rahul884061@users.noreply.github.com>
2024-11-28 09:52:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def learn():
         return jsonify({'error': str(e)})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+    app.run(debug=True, host='0.0.0.0', port=port)
